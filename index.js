5a75e8f868664e4c3ae9fd42a45b570603332196
potatofucker447 <156436165+potatofucker447@users.noreply.github.com>
2026-04-09 21:56:04 +0300

    no
    no

---
+++ index.js
@@ -0,0 +1,28 @@
+const mineflayer = require('mineflayer')
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'YOUR_SERVER_IP',
+    port: 25565,
+    username: 'AFK_Bot',
+    auth: 'offline'
+  })
+
+  bot.on('spawn', () => {
+    console.log('Joined server')
+  })
+
+  bot.on('end', () => {
+    console.log('Reconnecting...')
+    setTimeout(createBot, 5000)
+  })
+
+  bot.on('error', console.log)
+}
+
+createBot()
+
+// keeps hosting service alive
+require('http').createServer((req, res) => {
+  res.end('alive')
+}).listen(3000)
